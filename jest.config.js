ddc1c3e3f7ccd391b2ccbaaba4cb195b34db6c86
Jerick Wong <42172510+JerickWong@users.noreply.github.com>
2020-08-03 00:24:31 +0800

    unit tests - testing na nalimot icommit

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
+const {defaults} = require('jest-config');
+
 module.exports = {
 	collectCoverage: true,
 	collectCoverageFrom: ['src/**/*.{js,jsx}'],
+	moduleFileExtensions: [...defaults.moduleFileExtensions, 'js', 'jsx', 'svg'],
+	transform: {
+		  "\\.js$": "./node_modules/babel-jest",
+		  "^.+\\.svg$": "./svgTransform.js"
+		},
+	moduleNameMapper: {
+		'^.+\\.(css|less)$': 'CSSStub.js'
+	}
 };
