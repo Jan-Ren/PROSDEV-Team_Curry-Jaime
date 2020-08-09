7d6fcb5a2fbc01ceab823c06f932a442192c6746
Jan-Ren <46782934+Jan-Ren@users.noreply.github.com>
2020-08-09 14:56:17 +0800

    Save and edit PRF
    Saving and editting PRF is done (note: there is no PO attribute in PRF yet)

--- server/models/PRF.js
+++ server/models/PRF.js
@@ -29,13 +29,31 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const PRF = new Schema(
-    {
-        name: { type: String, required: true },
-        time: { type: [String], required: true },
-        rating: { type: Number, required: true },
+const PRF = new Schema({
+    prf_number: {
+        type: Number, 
+        required:true
     },
-    { timestamps: true },
-)
+    // po_number: Number,
+    pax: [{
+        type: String,
+        require:true
+    }],
+    receipient: String,
+    date_created: {
+        type: Date,
+        default: Date.now
+    },
+    last_modified: {
+        type: Date,
+        default: Date.now
+    },
+    paid_date: Date,
+    particulars: String,
+    php: Number,
+    prepared_by: String,                                      
+    approved_by: String,
+    received_by: String
+})
 
 module.exports = mongoose.model('prf', PRF)
