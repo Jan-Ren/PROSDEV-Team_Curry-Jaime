4cd12aec8943ec9280de03218e27325e200c96dc
Jerick Wong <42172510+JerickWong@users.noreply.github.com>
2020-09-11 00:43:49 +0800

    can uncancel

--- src/views/AdminCancelledPRF.jsx
+++ src/views/AdminCancelledPRF.jsx
@@ -25,7 +25,8 @@ import { prfHArray, prfDArray } from "variables/Variables.jsx";
 import DateInput from "components/DatePicker/DatePicker.jsx"
 import api from "../api";
 import moment from 'moment'
-
+import CircularProgress from '@material-ui/core/CircularProgress';
+import ConfirmationDialog from '../components/ConfirmationDialog/ConfirmationDialog.jsx'
 
 
 class AdminCancelledPRF extends Component {
@@ -47,7 +48,8 @@ class AdminCancelledPRF extends Component {
       const PRF = await (await api.getCancelledPRF()).data.data
       this.setState({ PRF, loading: false })
     } catch (error) {
-      
+      console.log(error)
+      this.setState({ loading: false })
     }
   }
 
@@ -56,11 +58,24 @@ class AdminCancelledPRF extends Component {
   }
 
   handleUncancel = async (prf) => {
+    this.setState({ isLoading: true, open: true, action: 'Uncancel' })
+    
+    prf.is_cancelled = false
+    prf.last_modified = Date.now()
+    try {
+      await api.updatePRFById(prf._id, prf)
 
+      setTimeout(() => {
+        this.setState({ isLoading: false, success: true })
+      }, 1500)
+    } catch (error) {
+      alert(error)
+    }
   }
 
   handleClose = () => {
-
+    this.setState({ open:false });
+    window.location.reload()
   }
 
   render() {
@@ -95,36 +110,52 @@ class AdminCancelledPRF extends Component {
                     </Form>
                   </Col>
                   <div>
-
-                  <Table striped hover>
-                    <thead>
-                      <tr>
-                        {prfHArray.map((prop, key) => {
-                          return <th key={key}>{prop}</th>;
-                        })}
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {this.state.PRF.map((prop, key) => {
-                        return (
-                          <tr key={key}>
-                              
-                              <td key={key+1}>{prop.prf_number}</td>
-                              <td key={key+2}>{prop.recipient}</td>
-                              <td key={key+4}>{moment(prop.paid_date).format('MM-DD-YYYY')}</td>
-                              <td key={key+4}>{moment(prop.date_created).format('MM-DD-YYYY hh:mm:ss A')}</td>
-                              <td key={key+5}>{moment(prop.last_modified).format('MM-DD-YYYY hh:mm:ss A')}</td>
-                              <td>
-                            <Link to={{pathname: '/create/New-PRF', state: {PRF: prop}}  } style={{ color: "inherit"}} ><Button variant="outline-secondary"><i className="pe-7s-look" />View</Button>{' '}</Link>
-                            <Button variant="outline-primary" bsStyle="success" onClick={() => this.handleUncancel(prop)}><i className="pe-7s-back-2"/> Uncancel</Button>{' '}
-                            <Button variant="outline-primary" bsStyle="danger" onClick={() => this.handleDelete(prop)}><i className="pe-7s-close-circle"/>Delete</Button>{' '}
-                            </td>
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                    
-                  </Table>
+                  {
+                    this.state.loading ?
+                    <div style={{padding: "100px 0", textAlign: "center"}}>
+                        <CircularProgress />
+                    </div> : 
+                    <Table striped hover>
+                      <thead>
+                        <tr>
+                          {prfHArray.map((prop, key) => {
+                            return <th key={key}>{prop}</th>;
+                          })}
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {
+                          !this.state.PRF.length ?
+                          <p>This list is empty.</p>
+                          :
+                          this.state.PRF.map((prop, key) => {
+                            return (
+                              <tr key={key}>
+                                  
+                                  <td key={key+1}>{prop.prf_number}</td>
+                                  <td key={key+2}>{prop.recipient}</td>
+                                  <td key={key+4}>{moment(prop.paid_date).format('MM-DD-YYYY')}</td>
+                                  <td key={key+4}>{moment(prop.date_created).format('MM-DD-YYYY hh:mm:ss A')}</td>
+                                  <td key={key+5}>{moment(prop.last_modified).format('MM-DD-YYYY hh:mm:ss A')}</td>
+                                  <td>
+                                <Link to={{pathname: '/create/New-PRF', state: {PRF: prop}}  } style={{ color: "inherit"}} ><Button variant="outline-secondary"><i className="pe-7s-look" />View</Button>{' '}</Link>
+                                <Button variant="outline-primary" bsStyle="success" onClick={() => this.handleUncancel(prop)}><i className="pe-7s-back-2"/> Uncancel</Button>{' '}
+                                <Button variant="outline-primary" bsStyle="danger" onClick={() => this.handleDelete(prop)}><i className="pe-7s-close-circle"/>Delete</Button>{' '}
+                                </td>
+                              </tr>
+                            );
+                          })}
+                      </tbody>
+                      
+                    </Table>
+                  }
+                  <ConfirmationDialog
+                    open={this.state.open}
+                    handleClose={this.handleClose}
+                    success={this.state.success}
+                    isLoading={this.state.isLoading}
+                    action={this.state.action}
+                    />
                   </div>
                 
                   </React.Fragment>
