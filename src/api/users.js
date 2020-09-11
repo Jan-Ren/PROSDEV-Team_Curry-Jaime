fd059b4c3c2a4d2296aa38d71eddbfd744203063
Jan-Ren <46782934+Jan-Ren@users.noreply.github.com>
2020-09-11 16:16:26 +0800

    Settings
    Change of admin and employee password

--- src/api/users.js
+++ src/api/users.js
@@ -7,18 +7,22 @@ const user = axios.create({
 export const login = payload => user.post(`/login-user`, payload)
 export const logout = () => user.get(`/logout-user`)
 export const getUser = payload => user.post(`/current-user`, payload)
+export const getAllUser = () => user.get(`/All-User`)
 export const register = payload => user.post(`/register-user`, payload)
 export const getJWT = () => user.get(`/get-jwt`)
 export const updatePRF_Folder = payload => user.put(`/update-PRF_Folder`, payload)
 export const updatePO_Folder = payload => user.put(`/update-PO_Folder`, payload)
+export const updatePassword = payload => user.put(`/update-password`, payload)
 const users = {
     login,
     logout,
     getUser,
+    getAllUser,
     register,
     getJWT,
     updatePRF_Folder,
-    updatePO_Folder
+    updatePO_Folder,
+    updatePassword
 }
 
 export default users
